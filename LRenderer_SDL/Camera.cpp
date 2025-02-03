0571ef42d261dc7066710aa9c88df97707e708a3
LanQ <2545493686@qq.com>
2025-02-03 21:41:40 +0800

    Camera::GetProjectionMatrix()

--- LRenderer_SDL/Camera.cpp
+++ LRenderer_SDL/Camera.cpp
@@ -1,4 +1,4 @@
-#include "Camera.h"
+﻿#include "Camera.h"
 
 Camera::Camera(Transform* transform)
 {
@@ -11,4 +11,27 @@ EIGEN_ALWAYS_INLINE Eigen::Matrix4f Camera::GetViewMatrix() const
 	affine.translate(-transform->position);
 	affine.rotate(transform->rotation.inverse());
 	return affine.matrix();
+}
+
+// 从 games101 作业抄的; LANQ 25.2.3
+Eigen::Matrix4f Camera::GetProjectionMatrix() const
+{
+    Eigen::Matrix4f p = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f mortho = Eigen::Matrix4f::Identity();
+
+    float nSize = tan(fov / 2 * M_PI / 180) * zNear;
+
+    p <<    zNear,  0,      0,              0,
+            0,      zNear,  0,              0,
+            0,      0,      zNear + zFar,   -zFar * zNear,
+            0,      0,      1,              0;
+
+    mortho <<   1 / (aspect * nSize),   0,          0,                  0,
+                0,                      1 / nSize,  0,                  0,
+                0,                      0,          2 / (zFar - zNear), -(zFar + zNear) / 2,
+                0,                      0,          0,                  1;
+
+    Eigen::Matrix4f projection = mortho * p;
+
+    return projection;
 }
--- LRenderer_SDL/Camera.h
+++ LRenderer_SDL/Camera.h
@@ -1,13 +1,22 @@
 #pragma once
+#define _USE_MATH_DEFINES
+
 #include "Component.h"
 #include "Transform.h"
 #include "Eigen/Dense"
+#include "cmath"
 
 class Camera : Component
 {
 public:
+	float fov = 45;
+	float aspect = 1;
+	float zNear = 0.1f;
+	float zFar = 50;
+
 	Camera(Transform* transform);
 	Eigen::Matrix4f GetViewMatrix() const;
+	Eigen::Matrix4f GetProjectionMatrix() const;
 
 private:
 	Transform *transform;
