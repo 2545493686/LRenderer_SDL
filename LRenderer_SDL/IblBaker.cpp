ab8d94ce208c2c54ff1aa04ed22d47bb244a239c
LanQ <2545493686@qq.com>
2025-03-20 15:14:26 +0800

    多线程化 Irradiance 生成

--- LRenderer_SDL/IblBaker.cpp
+++ LRenderer_SDL/IblBaker.cpp
@@ -1,45 +1,52 @@
 ﻿#include "IblBaker.h"
 
+#include <omp.h>
+
 #include "Cubemap.h"
 #include "Random.h"
 
 Cubemap *IblBaker::BakeIrradiance(Cubemap *input)
 {
-	static auto randomProvider = Random::InCircle(1.0f);
 	Cubemap *output = new Cubemap(input->GetSize() / 4);
 	
-	for (size_t face = 0; face < 6; face++)
+	#pragma omp parallel
 	{
-		for (size_t i = 0; i < output->GetSize(); i++)
+		auto randomProvider = Random::InCircle(1.0f);
+
+		#pragma omp for collapse(2) 
+		for (int face = 0; face < 6; face++)
 		{
-			for (size_t j = 0; j < output->GetSize(); j++)
+			for (int i = 0; i < output->GetSize(); i++)
 			{
-				Eigen::Vector2f uv;
-				uv.x() = static_cast<float>(i) / output->GetSize();
-                uv.y() = static_cast<float>(j) / output->GetSize();
-				
-				auto dir = output->GetDirection(static_cast<Cubemap::Face>(face), uv);
-				Eigen::Vector3f tempDir = dir + Eigen::Vector3f::Ones();
-
-				Eigen::Matrix3f tangentSpace;
-				tangentSpace.col(0) = dir.cross(tempDir).normalized();
-                tangentSpace.col(1) = tangentSpace.col(0).cross(dir).normalized();
-                tangentSpace.col(2) = dir;
-
-				int sampleCount = 64;
-				Eigen::Vector4f color = Eigen::Vector4f::Zero();
-				for (size_t i = 0; i < sampleCount; i++)
+				for (int j = 0; j < output->GetSize(); j++)
 				{
-					auto randomVectorInCircle = randomProvider.Pop();
-					auto z = std::sqrtf(1 - randomVectorInCircle.squaredNorm());
+					Eigen::Vector2f uv;
+					uv.x() = static_cast<float>(i) / output->GetSize();
+					uv.y() = static_cast<float>(j) / output->GetSize();
 
-					Eigen::Vector3f sampleVector;
-					sampleVector << randomVectorInCircle, z;
-                    sampleVector = tangentSpace * sampleVector;
-					color += input->Sample(sampleVector) / sampleCount;
+					auto dir = output->GetDirection(static_cast<Cubemap::Face>(face), uv);
+					Eigen::Vector3f tempDir = dir + Eigen::Vector3f::Ones();
+
+					Eigen::Matrix3f tangentSpace;
+					tangentSpace.col(0) = dir.cross(tempDir).normalized();
+					tangentSpace.col(1) = tangentSpace.col(0).cross(dir).normalized();
+					tangentSpace.col(2) = dir;
+
+					int sampleCount = 4096 * 16;
+					Eigen::Vector4f color = Eigen::Vector4f::Zero();
+					for (size_t k = 0; k < sampleCount; k++)
+					{
+						auto randomVectorInCircle = randomProvider.Pop();
+						auto z = std::sqrtf(1 - randomVectorInCircle.squaredNorm());
+
+						Eigen::Vector3f sampleVector;
+						sampleVector << randomVectorInCircle, z;
+						sampleVector = tangentSpace * sampleVector;
+						color += input->Sample(sampleVector) / sampleCount;
+					}
+
+					output->PutPixel(static_cast<Cubemap::Face>(face), uv, color);
 				}
-				
-				output->PutPixel(static_cast<Cubemap::Face>(face), uv, color);
 			}
 		}
 	}
